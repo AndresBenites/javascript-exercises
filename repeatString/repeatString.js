1e2ad0af9ddf587be3985582aeba36be821cf30e
Andres Benites <andres@benites.org>
2020-08-14 15:33:30 -0700

    finished coding

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeat) {
+    let returnString = "";
+    for (i=0; i < repeat; i++){
+        returnString += str;
+    }
+    return returnString;
 }
 
 module.exports = repeatString
