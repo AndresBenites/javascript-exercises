10d6e06556dfd77768594771bc038d567b788873
Andres Benites <andres@benites.org>
2020-08-14 14:56:24 -0700

    Finished coding

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, str) {
 
+    let returnArray = Array.from(myArray).filter(e => e !== str);
+    return returnArray;
 }
 
 module.exports = removeFromArray
