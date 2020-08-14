5466939ce3a6f8b6eccd63c531089284c301ac95
Andres Benites <andres@benites.org>
2020-08-14 15:44:44 -0700

    finished coding

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(myString) {
 
+    myArray = myString.split("");
+    myArray = myArray.reverse();
+    let returnStr = myArray.join("");
+    return returnStr;
 }
 
 module.exports = reverseString
