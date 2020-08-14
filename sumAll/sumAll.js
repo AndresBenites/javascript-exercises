fa93d7eeba0f353c26338e966f9f42b9ee97b06e
Andres Benites <andres@benites.org>
2020-08-14 15:04:01 -0700

    finished coding

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    let mySum=0;
+
+    for (i=start; i <= end; i++){
+        mySum += parseInt(i);
+    }
+    return mySum;
 }
 
 module.exports = sumAll
