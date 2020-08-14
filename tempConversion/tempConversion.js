a650b8c64739cd868cf5ef74d1ddf366967c53de
Andres Benites <andres@benites.org>
2020-08-14 15:27:44 -0700

    finished coding

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
+const ftoc = function(myTemp) {
+  let returnTemp = 0;
+
+  returnTemp = (myTemp - 32) * (5/9);
+  return roundToTwo(returnTemp);
 
 }
 
-const ctof = function() {
+const ctof = function(myTemp) {
+  let returnTemp = 0;
+  returnTemp = (myTemp) *(9/5) +32;
+  return roundToTwo(returnTemp);
+}
 
+function roundToTwo(num){
+  return +(Math.round(num + "e+2") + "e-2");
 }
 
 module.exports = {
