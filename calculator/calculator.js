b3b82a3d768a423b0cdbf07b1e752b725a3892e3
Andres Benites <andres@benites.org>
2020-08-19 19:59:38 -0700

    finished calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,49 @@
-function add () {
-	
+function add(a, b) {
+  return a + b;
 }
 
-function subtract () {
-	
+function subtract(a, b) {
+  return a - b;
 }
 
-function sum () {
-	
+function sum(arr) {
+	let theSum = 0;
+	for (i=0; i<arr.length; i++){
+		theSum += parseInt(arr[i]);
+	}
+	return theSum;
 }
 
-function multiply () {
-	
+function multiply(arr) {
+	let myX = 1;
+	for(i=0; i< arr.length; i++){
+		myX *= parseInt(arr[i]);
+	}
+	return myX;
 }
 
-function power() {
-	
+function power(a, b) {
+	pow = 1;
+	for (i=0; i<b;i++){
+		pow *= a;
+	}
+	return pow;
 }
 
-function factorial() {
-	
+function factorial(a) {
+	let myFX = 1;
+	for(i=a; i > 0; i--){
+		myFX *= i;
+	}
+	if (myFX == 0) return 1;
+	return myFX;
 }
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
