a4adfdb6f5aa20aaffd233c711de4e22b226d873
Andres Benites <andres@benites.org>
2020-08-20 15:32:19 -0700

    Finished coding

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase();
+    str = str.replace(/\s+/g, "");
+    let revStr = str.split("").reverse();
+    revStr = revStr.toString().replace(/,/g, "");
+    if (str == revStr) return true;
+    return false;
 }
 
 module.exports = palindromes
