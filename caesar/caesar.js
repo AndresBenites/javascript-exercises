e6f1c39582df06d46b2bb334683f6ac938c6d2ea
Andres Benites <andres@benites.org>
2020-08-20 19:39:04 -0700

    does not wrap around yet

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,26 @@
-const caesar = function() {
+const caesar = function (str, int) {
+  let newStr;
+  let strArray = Array.from(str);
+  let encStr = "";
+  let letter = "";
+  for (i = 0; i < strArray.length; i++) {
+    letter = strArray[i];
+    if (letter.charCodeAt(0) < 48) {
+        strArray[i] = strArray[i].charCodeAt(0);
+    } else {
+      strArray[i] = strArray[i].charCodeAt(0) + int;
+    }
+  }
+  for (i = 0; i < strArray.length; i++) {
+    if (String.fromCharCode(strArray[i]) == undefined) {
+      continue;
+    }
+    console.log(String.fromCharCode(strArray[i]));
+    encStr += String.fromCharCode(strArray[i]);
+  }
+  return encStr;
+};
 
-}
+caesar("Hello, World!", 5);
 
-module.exports = caesar
+module.exports = caesar;
