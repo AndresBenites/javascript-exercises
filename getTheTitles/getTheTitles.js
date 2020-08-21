c1275ceeaea63477108a8a8aec4dc5570df46505
Andres Benites <andres@benites.org>
2020-08-21 14:57:36 -0700

    finished

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookArr) {
+    bookTitles = [];
+    for (i=0; i < bookArr.length; i++){
+        bookTitles[i] = bookArr[i].title;
+    }
+    return bookTitles;
 }
 
 module.exports = getTheTitles;
