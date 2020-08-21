a1a73c755901dcc7e5cc451ce388388c17b0a117
Andres Benites <andres@benites.org>
2020-08-20 22:25:24 -0700

    Needs refactor badly, but it's working

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,21 +1,58 @@
-const caesar = function (str, int) {
+const caesar = function (str, int = 0) {
   let newStr;
   let strArray = Array.from(str);
   let encStr = "";
   let letter = "";
+  let wrapperInt = 0;
   for (i = 0; i < strArray.length; i++) {
     letter = strArray[i];
-    if (letter.charCodeAt(0) < 48) {
+    // letters less than A
+    if (letter.charCodeAt(0) < 65) {
       strArray[i] = strArray[i].charCodeAt(0);
-    } else if (letter.charCodeAt(0) > 90 && letter.charCodeAt(0) < 97 ||
-      letter.charCodeAt(0) > 122){
-      console.log(`letter: ${letter.charCodeAt(0)}`);
+    } else if (
+      // Letters above Z but below a and above z
+      (letter.charCodeAt(0) > 90 && letter.charCodeAt(0) < 97) ||
+      letter.charCodeAt(0) > 122
+    ) {
       strArray[i] = strArray[i].charCodeAt(0);
+      // letters from A-Z
+    } else if (letter.charCodeAt(0) >= 65 && letter.charCodeAt(0) <= 90) {
+      // letter + int above Z
+      if (letter.charCodeAt(0) + int > 90) {
+        wrapperInt = int;
+        while (letter.charCodeAt(0) + wrapperInt > 90) {
+          wrapperInt -= 26;
+        }
+        strArray[i] = strArray[i].charCodeAt(0) + wrapperInt;
+        // letter + (-int) below A
+      } else if (letter.charCodeAt(0) + int < 65) {
+        wrapperInt = int;
+        while (letter.charCodeAt(0) + wrapperInt < 65) {
+          wrapperInt += 26;
+        }
+        strArray[i] = strArray[i].charCodeAt(0) + wrapperInt;
+      } else {
+        strArray[i] = strArray[i].charCodeAt(0) + int;
+      }
+      // should all be a-z
     } else {
-      strArray[i] = strArray[i].charCodeAt(0) + int;
+      if (letter.charCodeAt(0) + int > 122) {
+        wrapperInt = int;
+        while (letter.charCodeAt(0) + wrapperInt > 122) {
+          wrapperInt -= 26;
+        }
+        strArray[i] = strArray[i].charCodeAt(0) + wrapperInt;
+      } else if (letter.charCodeAt(0) + int < 97){
+        wrapperInt = int;
+        while (letter.charCodeAt(0) + wrapperInt < 97) {
+          wrapperInt += 26;
+        }
+        strArray[i] = strArray[i].charCodeAt(0) + wrapperInt;        
+      } else {
+        strArray[i] = strArray[i].charCodeAt(0) + int;
+      }
     }
   }
-
 
   for (i = 0; i < strArray.length; i++) {
     if (String.fromCharCode(strArray[i]) == undefined) {
@@ -25,7 +62,5 @@ const caesar = function (str, int) {
   }
   return encStr;
 };
-
-caesar("Hello,[/ World!", 5);
 
 module.exports = caesar;
