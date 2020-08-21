ad91d3ddb3b626978777d63b50ed1ea9f19ef110
Andres Benites <andres@benites.org>
2020-08-20 20:54:31 -0700

    still working

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -6,21 +6,26 @@ const caesar = function (str, int) {
   for (i = 0; i < strArray.length; i++) {
     letter = strArray[i];
     if (letter.charCodeAt(0) < 48) {
-        strArray[i] = strArray[i].charCodeAt(0);
+      strArray[i] = strArray[i].charCodeAt(0);
+    } else if (letter.charCodeAt(0) > 90 && letter.charCodeAt(0) < 97 ||
+      letter.charCodeAt(0) > 122){
+      console.log(`letter: ${letter.charCodeAt(0)}`);
+      strArray[i] = strArray[i].charCodeAt(0);
     } else {
       strArray[i] = strArray[i].charCodeAt(0) + int;
     }
   }
+
+
   for (i = 0; i < strArray.length; i++) {
     if (String.fromCharCode(strArray[i]) == undefined) {
       continue;
     }
-    console.log(String.fromCharCode(strArray[i]));
     encStr += String.fromCharCode(strArray[i]);
   }
   return encStr;
 };
 
-caesar("Hello, World!", 5);
+caesar("Hello,[/ World!", 5);
 
 module.exports = caesar;
