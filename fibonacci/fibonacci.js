c5898b80342bb56ecd88b6aa7dfad12b1f33b161
Andres Benites <andres@benites.org>
2020-08-21 14:40:13 -0700

    finished for all positive numbers

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    numArray = [0, 1, 1]
+    if (num <= 2) return numArray[num];
+    for(i = 2; i < num; i++){
+        numArray[i+1] = numArray[i] + numArray[i-1];
+    }
+    return numArray[num];
 }
+
+console.log(fibonacci(3));
 
 module.exports = fibonacci
