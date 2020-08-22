72c629ccfd6235c32eb34d221e4032be71241d2f
Andres Benites <andres@benites.org>
2020-08-21 17:52:20 -0700

    finished code

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,35 @@
-let findTheOldest = function() {
+// const people = [
+//   {
+//     name: "Carly",
+//     yearOfBirth: 1942,
+//   },
+//   {
+//     name: "Ray",
+//     yearOfBirth: 1962,
+//     yearOfDeath: 2011,
+//   },
+//   {
+//     name: "Jane",
+//     yearOfBirth: 1912,
+//     yearOfDeath: 1941,
+//   },
+// ];
 
-}
+let findTheOldest = function (peopleObj) {
+  ;
+  let year = parseInt(new Date().getFullYear());
 
-module.exports = findTheOldest
+  for (i = 0; i < Object.keys(peopleObj).length; i++) {
+    if (typeof peopleObj[i].yearOfDeath === "undefined") {
+      peopleObj[i].age = year - peopleObj[i].yearOfBirth;
+      continue;
+    }
+    peopleObj[i].age = peopleObj[i].yearOfDeath - peopleObj[i].yearOfBirth;
+  }
+  let returnPerson = Array.from(peopleObj).reduce((a, b) => a.age > b.age ? a : b)
+  return returnPerson;
+};
+
+// findTheOldest(people);
+
+module.exports = findTheOldest;
